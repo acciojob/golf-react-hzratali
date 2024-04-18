6ea86f240b74f58db45b09240aac9ad39d364010
Hazrat Ali <hazrat8143@gmail.com>
2024-04-18 18:48:40 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,51 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      ballPosition: { left: "0px" },
     };
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+  buttonClickHandler() {
+    this.setState({
+      renderBall: true,
+    });
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
+  handleKeyDown(event) {
+    if (event.keyCode === 39) {
+      // Right arrow key
+      const newPos = parseInt(this.state.ballPosition.left) + 5;
+      this.setState({
+        ballPosition: { left: `${newPos}px` },
+      });
     }
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  render() {
+    return (
+      <div className="playground">
+        {this.state.renderBall ? (
+          <div className="ball" style={this.state.ballPosition}></div>
+        ) : (
+          <button className="start" onClick={this.buttonClickHandler}>
+            Start
+          </button>
+        )}
+      </div>
+    );
+  }
 }
-
 
 export default App;
